11fd188599c83c003501995b65ec79b743d0683b
Dylansweaver <dylansweaver@gmail.com>
2016-12-02 11:26:35 -0600

    2 pins

--- Contents.swift
+++ Contents.swift
@@ -4,26 +4,26 @@ import XCPlayground
 import Foundation
 //Manually Enter Destinations and stops here
 var destinations: [[String:Any]] =
-[
-    
-    //Destination 1: California
     [
-        "Destination": "California",//final destination
-        "Stops": "McDonalds\n"+"Las Vegas",//stops along the way
-        "Cost": 3000,//the cost in USD for the entire trip
-        "Distance" : 500,//the total distance planned to travel one way
-        "Bring": " Shorts\n T-Shirts\n Swimwear\n Sunblock"//checklist of things to bring along on the trip
-    ],
-    
-    //Destination 2: Hawaii
-    [
-        "Destination":"Hawaii",
-        "Stops": "Dunkin Donuts\n Ferry",
-        "Cost": 5000,
-        "Distance": 2000,
-        "Bring": " Shorts\n T-Shirts\n Swimwear\n Sunblock\n Camera"
-    ]
-    //Add more Destiations here if Needed
+        
+        //Destination 1: California
+        [
+            "Destination": "California",//final destination
+            "Stops": "McDonalds\n"+"Las Vegas",//stops along the way
+            "Cost": 3000,//the cost in USD for the entire trip
+            "Distance" : 500,//the total distance planned to travel one way
+            "Bring": " Shorts\n T-Shirts\n Swimwear\n Sunblock"//checklist of things to bring along on the trip
+        ],
+        
+        //Destination 2: Hawaii
+        [
+            "Destination":"Hawaii",
+            "Stops": "Dunkin Donuts\n Ferry",
+            "Cost": 5000,
+            "Distance": 2000,
+            "Bring": " Shorts\n T-Shirts\n Swimwear\n Sunblock\n Camera"
+        ]
+        //Add more Destiations here if Needed
 ];
 
 //manually enter which Destination you want to see
@@ -82,37 +82,45 @@ print(destinations[choice - 1]["Distance"]!,//~~
 
 
 
-
 //||||||||||||||||||||||||||||||||MESSING WITH MAPKIT|||||||||||||||||||||
-let pin = CLLocationCoordinate2DMake(34.0522342,-118.243)//this is to set the lattitude and longititude
-
+let finalPin = CLLocationCoordinate2DMake(34.0522342,-118.243)//this is to set the final location
+let stop1 = CLLocationCoordinate2DMake(34.0822342,-118.343)
 
 // set the zoom
 let delta = 1.75//sets the zoom into the map
+
 
 // set the size of the map
 let frame = CGRect( x:0, y:0, width:400, height:400 )//creates the fram for the map to be displayed in
 let mapView = MKMapView( frame: frame )//also for the frame
 
+
 // create and populate a coordinate region struct
 var region = MKCoordinateRegion()//for putting the location on the map
 //region.center.latitude = 34.0522342 //if wanten to do lat/long seperate
 //region.center.longitude = -118.243
-region.center = pin //uses the location declared above
+region.center = finalPin //uses the location declared above
+
 
 // span defines the zoom
 region.span.latitudeDelta = delta
 region.span.longitudeDelta = delta
 
+
 // inform the mapView of these edits
 mapView.setRegion( region, animated: true )
-//annotation
-let annotation = MKPointAnnotation()
-annotation.coordinate = pin
-annotation.title = "Pin"
-annotation.subtitle = "Somerville, MA"
 
+
+//annotations
+let annotation2 = MKPointAnnotation()
+annotation2.coordinate = stop1
+annotation2.title = "Stop 1"
+let annotation = MKPointAnnotation()
+annotation.coordinate = finalPin
+annotation.title = "Final Destination"
+annotation.subtitle = "LA, CA"
+
+mapView.addAnnotation(annotation2)
 mapView.addAnnotation(annotation)
 // view the map in the timeline!
 XCPlaygroundPage.currentPage.liveView = mapView
-
